d49763aed4b2c225673107940c20a68d0194ae3a
Martin Steih <martinsteih@gmx.de>
2022-10-31 16:12:29 -0600

    library only project
    Signed-off-by: Martin Steih <martinsteih@gmx.de>

--- include/pdu.hpp
+++ include/pdu.hpp
@@ -6,6 +6,10 @@
 
 namespace fsoe {
 
+/**
+ * @brief
+ *
+ */
 enum class Command : uint8_t {
   ProcessData = 0x36,
   Reset = 0x2A,
@@ -15,20 +19,61 @@ enum class Command : uint8_t {
   FailSafeData = 0x08
 };
 
-struct safe_data {
+/**
+ * @brief
+ *
+ */
+struct SafeData {
   uint16_t _safe_data;
   uint16_t _crc;
 };
 
-struct pdu {
+/**
+ * @brief
+ *
+ */
+struct Pdu {
   Command _command;
-  std::vector<safe_data> _data;
+  std::vector<SafeData>
+      _data; /**<Variable number of pairs of two byte safety data, and two byte
+                crcs. Length is configured by the safety device description. */
   uint16_t _connection_id;
 };
 
+class Slave {
+ public:
+  void EventFrameReceived();
+  void EventWatchdogExpired();
+  void EventResetConnection();
+  void EventSetDataCommand();
+
+  void ActionSendFrame();
+
+ private:
+  uint16_t _connectionId;
+  uint16_t _sessionId;
+  uint16_t _lastCrC0;
+  uint16_t _sequenceNumber;
+};
+
+/**
+ * @brief
+ *
+ */
 constexpr uint32_t safety_polynoial{0x139B7};
 
-bool CalcCrc(pdu& packet, const uint16_t start_crc,
+/**
+ * @brief
+ *
+ * @param packet
+ * @param start_crc
+ * @param sequence_number
+ * @param old_crc
+ * @param rcv_direction
+ * @return true
+ * @return false
+ */
+bool CalcCrc(Pdu& packet, const uint16_t start_crc,
              const uint16_t sequence_number, const uint16_t old_crc,
              bool rcv_direction);
 
